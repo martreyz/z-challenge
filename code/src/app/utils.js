edaff5881319c9f20205d1d022f38bc82752ac9a
martreyz <martreyz@gmail.com>
2025-01-29 22:29:43 +0100

    feat: remove routes until needed

--- code/src/app/utils.js
+++
@@ -1,7 +0,0 @@
-export const verifyAPIKey = (res, requestApiKey) => {
-  const validApiKey = process.env.API_KEY;
-
-  if (requestApiKey !== validApiKey) {
-    return res.status(401).json({ error: "Unauthorized: Invalid API key" });
-  }
-};
