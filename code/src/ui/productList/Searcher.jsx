ce5bec38304bb3958476ea34c67baccf4b0869ef
martreyz <martreyz@gmail.com>
2025-01-29 21:12:27 +0100

    feat: add base layout

---
+++ code/src/ui/productList/Searcher.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Searcher = ()=> {
+  return (
+    <>
+        <form>
+            <input type="text" placeholder="Search for a smartphone..." />
+            <button type="submit">Search</button>
+        </form>
+
+    </>
+  );
+}
+
+export default Searcher;
