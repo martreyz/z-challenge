9b1a4d0bf90f9cc01d1f62d7af97a0e4ac746a38
martreyz <martreyz@gmail.com>
2025-01-30 00:07:18 +0100

    feat: update details to base layout

--- code/src/ui/productList/ProductListWrapper.jsx
+++ code/src/ui/productList/ProductListWrapper.jsx
@@ -1,11 +1,23 @@
 "use client";
 
 import Searcher from "@/ui/productList/Searcher";
+import { useState } from "react";
+import { useSmartphoneListContext } from "../contexts/SmartphoneListContext";
 
-const ProductListWrapper = ()=> {
+const ProductListWrapper = ({ allSmartphones }) => {
+  const { updateSmartphoneList, smartphoneList } = useSmartphoneListContext();
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchInputChange = (event) => {
+    console.log(handleSearch);
+  };
   return (
-    <Searcher />
+    <Searcher
+      handleSearchInputChange={handleSearchInputChange}
+      searchInput={searchInput}
+      numberOfResults={allSmartphones.length}
+    />
   );
-}
+};
 
 export default ProductListWrapper;
--- code/src/ui/productList/Searcher.jsx
+++ code/src/ui/productList/Searcher.jsx
@@ -1,15 +1,27 @@
 "use client";
 
-const Searcher = ()=> {
+import styles from "@/styles/searcher.module.css";
+
+const Searcher = ({
+  numberOfResults,
+  searchInput,
+  handleSearchInputChange,
+}) => {
   return (
     <>
-        <form>
-            <input type="text" placeholder="Search for a smartphone..." />
-            <button type="submit">Search</button>
-        </form>
-
+      <form>
+        <input
+          className={styles.searcherInput}
+          value={searchInput}
+          onChange={handleSearchInputChange}
+          type="text"
+          placeholder="Search for a smartphone..."
+        />
+        <button type="submit">Search</button>
+      </form>
+      <p className={styles.counter}>{numberOfResults} results</p>
     </>
   );
-}
+};
 
 export default Searcher;
