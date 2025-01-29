58029438dad5dbef28159d1693604a6e2405c27d
martreyz <martreyz@gmail.com>
2025-01-29 21:13:43 +0100

    feat: add base layout

--- code/src/app/page.js
+++ code/src/app/page.js
@@ -1,7 +1,26 @@
 import { getAllProducts } from "@/app/usecases/getAllProducts";
+import ProductListWrapper from "@/ui/productList/ProductListWrapper";
+import Image from "next/image";
 
 export default async function Home() {
   const allProducts = await getAllProducts();
 
-  return <span>This is the home page</span>;
+  console.log(allProducts);
+
+  return (
+    <>
+      <header>
+        <Image src="/assets/logo.svg" alt="Logo" width={77} height={30} />
+        <Image
+          src="/assets/bagIcon_mobile.svg"
+          alt="Bag"
+          width={18}
+          height={18}
+        />
+      </header>
+      <main>
+        <ProductListWrapper />
+      </main>
+    </>
+  );
 }
