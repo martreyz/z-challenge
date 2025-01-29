065c5affc14612aacab063ad163f3dc9437c2713
martreyz <martreyz@gmail.com>
2025-01-30 00:08:23 +0100

    feat: change entity format as per next recommendations

---
+++ code/src/infra/API/products.js
@@ -0,0 +1,21 @@
+import ProductForList from "@/domain/entities/ProductForList";
+
+export const fetchProducts = async (searchQuery) => {
+  const params = new URLSearchParams({
+    limit: 20,
+    ...(searchQuery && { search: searchQuery }),
+  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/products?${params}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.API_KEY,
+      },
+    }
+  );
+  const data = await response.json();
+
+  return data;
+};
