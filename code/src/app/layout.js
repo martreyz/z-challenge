9bd4077169411c4b8a472204bd13f5caa82f1cf6
martreyz <martreyz@gmail.com>
2025-02-01 07:43:13 +0100

    feat: change header to layout to share it between pages

--- code/src/app/layout.js
+++ code/src/app/layout.js
@@ -1,14 +1,33 @@
 import "../styles/main.css";
 
+import Image from "next/image";
+
 export const metadata = {
   title: "MBST",
   description: "Mobile Store",
 };
 
+const Header = () => {
+  return (
+    <header>
+      <Image src="/assets/logo.svg" alt="Logo" width={77} height={30} />
+      <Image
+        src="/assets/bagIcon_mobile.svg"
+        alt="Bag"
+        width={18}
+        height={18}
+      />
+    </header>
+  );
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- code/src/app/page.js
+++ code/src/app/page.js
@@ -1,25 +1,12 @@
 import { SmartphoneListProvider } from "@/ui/contexts/SmartphoneListContext";
-import ProductList from "@/ui/productList/SmartphoneList";
+import SmartphoneList from "@/ui/smartphoneList/SmartphoneList";
 import Searcher from "@/ui/searcher/Searcher";
-
-import Image from "next/image";
 
 export default async function Home() {
   return (
     <SmartphoneListProvider>
-      <header>
-        <Image src="/assets/logo.svg" alt="Logo" width={77} height={30} />
-        <Image
-          src="/assets/bagIcon_mobile.svg"
-          alt="Bag"
-          width={18}
-          height={18}
-        />
-      </header>
-      <main>
-        <Searcher />
-        <ProductList />
-      </main>
+      <Searcher />
+      <SmartphoneList />
     </SmartphoneListProvider>
   );
 }
