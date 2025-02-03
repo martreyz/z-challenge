cb177633405882c2f3e6e86b537385b6f9fb5701
martreyz <martreyz@gmail.com>
2025-02-03 19:05:56 +0100

    config: add react as required by vercel

--- code/src/app/page.js
+++ code/src/app/page.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { SmartphoneListProvider } from "@/ui/contexts/SmartphoneListContext";
 import SmartphoneList from "@/ui/smartphoneList/SmartphoneList";
 import Searcher from "@/ui/searcher/Searcher";
---
+++ code/src/ui/smartphoneDetail/SmartphoneDetailShoppingInfo.jsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { useSmartphoneDetailContext } from "@/ui/contexts/SmartphoneDetailContext";
+import styles from "@/styles/smartphoneDetail.module.css";
+
+import Image from "next/image";
+import { useState } from "react";
+import SmartphoneItem from "@/ui/smartphoneList/SmartphoneListItem";
+import SmartphoneForList from "@/domain/entities/SmartphoneForList";
+import { useShoppingCart } from "@/ui/contexts/ShoppingCartContext";
+import { useMessages } from "@/ui/hooks/useMessages";
+
+const SmartphoneDetail = () => {
+  const { smartphoneDetail } = useSmartphoneDetailContext();
+  const { addNewSmartphoneToCart } = useShoppingCart();
+
+  const messages = useMessages();
+
+  const { id, storageOptions, colorOptions, name, basePrice, similarProducts } =
+    smartphoneDetail;
+
+  const [selectedStorageOption, setSelectedStorageOption] = useState(undefined);
+
+  const [selectedColorOption, setSelectedColorOption] = useState(
+    colorOptions[0]
+  );
+
+  return (
+    <section className={styles.smartphoneDetail__shoppingInfo}>
+      <div className={styles.smartphoneDetail__imageWrapper}>
+        {selectedColorOption?.imageUrl ? (
+          <Image
+            src={selectedColorOption.imageUrl}
+            alt={`${name} in color ${colorOptions[0]?.name}`}
+            fill
+            style={{
+              objectFit: "scale-down",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        ) : (
+          messages("userMessage.imageNotAvailable")
+        )}
+      </div>
+      <span className={styles.smartphoneDetail__name}>{name}</span>
+      <span className={styles.smartphoneDetail__basePrice}>
+        {selectedStorageOption
+          ? selectedStorageOption.price
+          : messages("smartphoneDetail.priceFrom.label", { basePrice })}
+      </span>
+      <div className={styles.smartphoneDetail__storage}>
+        <span>{messages("smartphoneDetail.storage.label")}</span>
+        <form className={styles.smartphoneDetail__storageForm}>
+          {storageOptions?.map((option, i) => (
+            <label
+              className={styles.smartphoneDetail__storageLabel}
+              key={option.capacity + i}
+              style={{
+                "--dynamic-storageLabel-borderColor":
+                  selectedStorageOption?.capacity === option.capacity
+                    ? "#000000"
+                    : "#CCCCCC",
+              }}
+            >
+              <input
+                className={styles.smartphoneDetail__storageInput}
+                type="radio"
+                name={option.capacity}
+                value={option.capacity}
+                checked={selectedStorageOption?.capacity === option.capacity}
+                onChange={() => setSelectedStorageOption(option)}
+              />
+              {option.capacity}
+            </label>
+          ))}
+        </form>
+      </div>
+      <div className={styles.smartphoneDetail__color}>
+        <span>{messages("smartphoneDetail.color.label")}</span>
+        <form className={styles.smartphoneDetail__colorForm}>
+          {colorOptions?.map((option, i) => (
+            <label
+              className={styles.smartphoneDetail__colorLabel}
+              key={option.name + i}
+              style={{
+                "--dynamic-colorLabel-color": option.hexCode,
+                "--dynamic-colorLabel-borderColor":
+                  selectedColorOption?.name === option.name
+                    ? "#000000"
+                    : "#CCCCCC",
+              }}
+            >
+              <input
+                className={styles.smartphoneDetail__colorInput}
+                type="radio"
+                name={option.name}
+                value={option.name}
+                checked={selectedStorageOption?.name === option.name}
+                onChange={() => setSelectedColorOption(option)}
+              />
+            </label>
+          ))}
+        </form>
+        <span>{selectedColorOption?.name}</span>
+      </div>
+      <button
+        disabled={!selectedStorageOption}
+        className={styles.smartphoneDetail__addButton}
+        onClick={() => {
+          addNewSmartphoneToCart({
+            id,
+            name,
+            imageUrl: selectedColorOption.imageUrl,
+            color: selectedColorOption.name,
+            storage: selectedStorageOption.capacity,
+            price: selectedStorageOption.price,
+          });
+        }}
+      >
+        {messages("smartphoneDetail.addButton.label")}
+      </button>
+    </section>
+  );
+};
+
+export default SmartphoneDetail;
